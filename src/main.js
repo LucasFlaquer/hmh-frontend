fca239cbdfe2d559cf609113bae1a525c88f531d
lucasflaquer <lucas.flaquer@crbconstrutora.com.br>
2020-05-08 07:34:45 -0300

    ++

--- src/main.js
+++ src/main.js
@@ -5,13 +5,13 @@ import store from './store'
 import './scss/main.scss'
 import ProductList from '@/components/ProductList'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+Vue.config.productionTip = false
 
 Vue.component('ProductList', ProductList)
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
-Vue.config.productionTip = false
 
 new Vue({
   router,
