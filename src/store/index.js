a51b0ff5d80eac4b33758b3bbb73fb7df11aa3a2
lucasflaquer <lucas.flaquer@crbconstrutora.com.br>
2020-05-16 01:14:20 -0300

    api adicionada e imagens

--- src/store/index.js
+++ src/store/index.js
@@ -8,19 +8,66 @@ export default new Vuex.Store({
     products:[
       {
         id:1,
-        title:'Projeto 1',
-        description: 'lorem saofj asd´pfhijasdpóf ui´nvurnóidfidpoqwer rovmf-f g'
+        title:'Terreno + Obra',
+        description: 'Terreno + obra - Aqui na HMH você pode escolher projetos pré-estabelecidos pelo nosso arquiteto ou personalizar em conjunto com a gente o seu próprio projeto! ',
+        galery: [
+          {
+            id:1,
+            path:'projeto1/modelo-1.jpg'
+          },
+          {
+            id:2,
+            path:'projeto1/modelo-2.jpg'
+          },
+          {
+            id:3,
+            path:'projeto1/modelo-3.jpg'
+          },
+          {
+            id:4,
+            path:'projeto1/planta-baixa.jpg'
+          },
+        ]
       },
       {
         id:2,
-        title:'Projeto 2',
-        description: 'lorem saofj asd´pfhijasdpóf ui´nvurnóidfidpoqwer rovmf-f g'
-      },
-      {
-        id:3,
-        title:'Projeto 3',
-        description: 'lorem saofj asd´pfhijasdpóf ui´nvurnóidfidpoqwer rovmf-f g'
-      },
+        title:'Sobrados',
+        description: 'Aqui na HMH você tem a oportunidade de morar em um sobrado totalmente planejado e executado para você e sua família!',
+        galery:[
+          {
+            id:1,
+            path:'projeto2/img1.jpg'
+          },
+          {
+            id:2,
+            path:'projeto2/img2.jpeg'
+          },
+          {
+            id:3,
+            path:'projeto2/img3.jpeg'
+          },
+          {
+            id:4,
+            path:'projeto2/img4.jpeg'
+          },
+          {
+            id:5,
+            path:'projeto2/img5.jpeg'
+          },
+          {
+            id:6,
+            path:'projeto2/img6.jpeg'
+          },
+          {
+            id:7,
+            path:'projeto2/img7.jpeg'
+          },
+          {
+            id:8,
+            path:'projeto2/img8.jpeg'
+          }
+        ]
+      }
     ]
   },
   mutations: {
@@ -36,6 +83,7 @@ export default new Vuex.Store({
     },
     getProductById: state => id=> {
       return state.products.find(prod=> prod.id===id)
-    }
+    },
+    
   }
 })
---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="detail-product">
+    <header class="header container">
+      <a href="#" class="header__logo">
+        <h1>
+          <img src="../assets/logo-hmh.jpeg" alt="logo">
+        </h1>
+      </a>
+      <nav class="header__nav">
+        <a href="#projects">Projetos</a>
+        <a href="#about">Sobre</a>
+        <a href="#contacts">Contato</a>
+      </nav>
+    </header>
+    <div class="product">
+      <b-row>
+        <b-col>
+           <swiper class="swiper">
+            <swiper-slide v-for="item in product.galery" :key="item.id">
+              <img :src="intem.image" alt="" />
+              <p>{{ item.nome }}</p>
+            </swiper-slide>
+          </swiper>
+        </b-col>
+        <b-col>
+
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+  export default {
+    name:'ProductDetail',
+    components: {
+    Swiper,
+    SwiperSlide
+  },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
